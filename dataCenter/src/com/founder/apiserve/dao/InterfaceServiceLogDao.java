bc0fe60b22fbebe218a47bfbb56500c2b64d437d
yejianping <522620601@qq.com>
2015-04-20 14:45:54 +0800

    Revert "2"
    This reverts commit e770bcafe71633cb0a32fa01614b34dd390e8b2b.

--- dataCenter/src/com/founder/apiserve/dao/InterfaceServiceLogDao.java
+++
@@ -1,8 +0,0 @@
-package com.founder.apiserve.dao;
-
-import com.founder.apiserve.domain.InterfaceServiceLog;
-import com.founder.base.dao.BaseDao;
-
-public interface InterfaceServiceLogDao extends BaseDao<InterfaceServiceLog> {
-
-}
